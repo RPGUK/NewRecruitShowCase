7c89bd3b2f74ea724e9fff55a544fdb2075fe485
Ryan Glenn <ryan.g@expatustax.com>
2024-02-22 08:25:22 +0000

    Fix-Resharper-RenderMode-Syntax-Bug-Temporary

---
+++ Showcase.Blazor.WebServer/RenderModeInteractiveServer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Showcase.Blazor.WebServer
+{
+    public sealed class RenderModeInteractiveServer : RenderModeAttribute  
+    {
+        public override IComponentRenderMode Mode => (IComponentRenderMode)RenderMode.InteractiveServer;
+    }
+}
